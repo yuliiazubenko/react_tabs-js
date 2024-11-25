e4557cb5f6cc9a4e441d0d57643fa91ed259f57d
Yuliia Zubenko <95251336+yuliiazubenko@users.noreply.github.com>
2024-11-26 00:37:39 +0100

    final solution

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import 'bulma/css/bulma.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import './App.scss';
+import { Tabs } from './components/Tabs/Tabs';
+
+export const tabs = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
+
+export const App = () => {
+  const [activeTabId, setActiveTabId] = useState(tabs[0].id);
+  const activeTab = tabs.find(tab => tab.id === activeTabId);
+
+  return (
+    <div className="section">
+      <h1 className="title">Selected tab is {activeTab?.title}</h1>{' '}
+      <Tabs
+        tabs={tabs}
+        activeTabId={activeTabId}
+        onTabSelected={id => setActiveTabId(id)}
+      />
+    </div>
+  );
+};
